e3aee469d38df53e7fd44c28ee2b056d2230c21b
vladimir <nicenkovladimir@ukr.net>
2019-02-16 18:53:39 +0200

    server commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+const app = express();
+let heroes = [
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' }
+];
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, function() {
+	console.log(`server is running on port ${PORT}`); 
+});
+
+app.get('/get', function(req,res) {
+	if(req.body.id) {
+		res.send(heroes.find(hero => hero.id==req.body.id));
+	}
+	else if (req.body.name) {
+		res.send(heroes.filter(hero => hero.name.match(new ReqExp(req.body.name))));
+	}
+	else {
+		res.send(heroes);
+	}
+	res.status(404).send({'message': 'not found. 404'});
+});
+
+app.post('/post', function(req, res) {
+	let hero = req.body;
+	hero.id=heroes[heroes.length-1].id+1;
+	heroes.push(hero);
+	res.status(200).send(hero);
+	res.status(404).send({'message': 'not found. 404'});
+});
+
+app.put('/put', function(req, res) {
+	let heroId = Number(req.body.id);
+	let hero = heroes.find((el)=> el.id==heroId);
+	hero.name = req.body.name;
+	res.status(200).send('data has been updated');
+	res.status(404).send({'message': 'not found. 404'});
+});
+
+app.delete('/delete', function(req, res) {
+	let heroId = Number(req.body);
+	heroes.splice(heroId-1, 1);
+	res.status(200).send('data has been deleted');
+	res.status(404).send({'message': 'not found. 404'});
+});
